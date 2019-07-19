bbf85b92c68b1cad52c310803c5f67ad80a71236
KwanIkJang <44356083+rhksdlr134@users.noreply.github.com>
2019-07-19 13:55:23 +0900

    clear

--- day2/examples/Newclass.java
+++
@@ -1,7 +0,0 @@
-package day2.examples;
-
-public class Newclass {
-	public static void main(String[] args) {
-		System.out.println("hellooooooooooo");
-	}
-}
--- day2/examples/ZwooS2.java
+++
@@ -1,9 +0,0 @@
-package day2.examples;
-
-public class ZwooS2 {
-	
-	public static void main(String args[]) {
-		int num=5;
-		System.out.print(num);	
-	}
-}
